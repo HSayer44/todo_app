9983bc349b986b27679dc2e949a88416e8cb5604
Al Sayer <alsayerhaitham@gmail.com>
2024-04-02 15:05:43 +0200

    basic cubit methods

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+
+  CounterCubit() : super(0);
+
+  void increment() {
+    // print('Before: $state');
+    emit(state + 1); //update the state and notified all the listner about the state
+    // print('After: $state');
+  }
+
+  void decrement() {
+    emit(state - 1);
+  }
+}
