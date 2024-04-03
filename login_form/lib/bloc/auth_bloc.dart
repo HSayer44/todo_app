51a26f4c43b3fac1501d993430d13465a37efae8
Al Sayer <alsayerhaitham@gmail.com>
2024-04-03 11:04:23 +0200

    formatting

--- login_form/lib/bloc/auth_bloc.dart
+++ login_form/lib/bloc/auth_bloc.dart
@@ -4,36 +4,43 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        final email = event.email;
-        final password = event.password;
-        //Email validation
-        if(password.length < 6){
-          return emit(AuthFailure('Password can not be less than 6 charachters'));
-        }
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthSuccess(uid: '$email-$password'));
-        });
-      } catch (e) {
-        return emit(AuthFailure(e.toString()));
-      }
-    });
+    on<AuthLoginRequested>(_onAuthLoginRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
 
-    on<AuthLogoutRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthInitial());
-        });
-      } catch (e) {
-        emit(AuthFailure(e.toString()));
+
+
+
+  void _onAuthLoginRequested(AuthLoginRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
+//Email validation
+      if (password.length < 6) {
+        return emit(AuthFailure('Password can not be less than 6 charachters'));
       }
-    });
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthSuccess(uid: '$email-$password'));
+      });
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(AuthLogoutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
   }
 }
