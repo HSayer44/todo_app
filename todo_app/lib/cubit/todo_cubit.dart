35956c93a27781fa190243d1bc0d163f0627a699
Al Sayer <alsayerhaitham@gmail.com>
2024-04-03 08:19:41 +0200

    add log change

--- todo_app/lib/cubit/todo_cubit.dart
+++ todo_app/lib/cubit/todo_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/models/todo_model.dart';
 
@@ -6,7 +8,14 @@ class TodoCubit extends Cubit<List<Todo>> {
 
   void addTodo(String title) {
     final todo = Todo(name: title, createdAt: DateTime.now());
-    state.add(todo);
-    emit([...state]);
+    emit([...state, todo]);
+  }
+
+
+  //to log the changes in the state
+  @override
+  void onChange(Change<List<Todo>> change) {
+    super.onChange(change);
+    log(change.toString());
   }
 }
